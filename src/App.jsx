cfb10d01acf22b50bfe26aa6d10a3aec18525775
FVDB98 <felixvanderborgh@gmail.com>
2026-03-24 15:46:12 +0000

    Add back link to auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -184,6 +184,22 @@ function AuthPage({ mode }) {
       <section className="auth-layout auth-layout-minimal">
         <article className="auth-card auth-card-minimal">
           <div className="auth-page-title-banner">
+            <a className="auth-back-link" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                />
+              </svg>
+              Back
+            </a>
             <h1 className="auth-page-title">{authTitle}</h1>
           </div>
           <div className="auth-clerk-shell">
